74b3071054675910a291d17b3ac3947b08f6c44c
Eduardo L <centurionjaime@gmail.com>
2026-01-13 21:25:46 -0500

    fix: Cambiar el componente de layout de 'x-app-guest' a 'x-guest-layout' en las vistas de invitación

--- resources/views/invitations/expired.blade.php
+++ resources/views/invitations/expired.blade.php
@@ -1,4 +1,4 @@
-<x-app-guest>
+<x-guest-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Invitación Expirada') }}
@@ -98,5 +98,5 @@
             </div>
         </div>
     </div>
-</x-app-guest>
+</x-guest-layout>
 
